343ac1fa93efb133c2727184d96c60f91e0fb91b
Zebafarheensheikh <zebasheikhshiba@gmail.com>
2025-04-15 14:41:37 +0530

    strings reverse the string

--- src/AUtomationTesting/AssignmentFirst.java
+++ src/AUtomationTesting/AssignmentFirst.java
@@ -1,6 +1,8 @@
 package AUtomationTesting;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AssignmentFirst {
 
@@ -53,7 +55,37 @@ public class AssignmentFirst {
 	    {
 	    	System.out.println(variable_1);
 	    }
+	    //Item is present in the array list
+	    System.out.println(AL.contains("SCript"));
+	    System.out.println("**************************");
 	    
+	    //List 
+	    String[] name= {"ahan","Jahan,","Monday","Priya"};
+	    List<String> nameArrayList= Arrays.asList(name);
+	    nameArrayList.contains("Jahan");
+	    
+	    String s= "Ana Bell Harry Porter";
+	    List<String> nameArrayList1= Arrays.asList(s);
+	    String[] splittedString = s.split("Bell");
+	    System.out.println(splittedString[0]);
+	    System.out.println(splittedString[1]);
+	    //System.out.println(splittedString[2]);
+	    //System.out.println(splittedString[3]);
+	    System.out.println(splittedString[1].trim());
+	    
+	    for(int i=s.length()-1;i>=0;i--)
+	    {
+	    	System.out.println(s.charAt(i));
 	    }
+	    //Reverse the string values
+	    String ns= "Fisrt Second Third Fourth Fifth";
+	    
+	    for(int i=ns.length()-1; i>=0;i--)
+	    {
+	    	System.out.println(ns.charAt(i));
+	    }
+	    
+	    
+	}
 
 	}
